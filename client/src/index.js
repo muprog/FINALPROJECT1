d48a2ae5284e9027005d75d22616958e6c398113
muprog <128657551+muprog@users.noreply.github.com>
2024-10-29 14:55:45 +0300

    3rd

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,30 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+// import './i18n'
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './i18n'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import './i18n';
+
+// Use ReactDOM.render instead of createRoot
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')  // Pass the element as a second argument
 );
 
 
